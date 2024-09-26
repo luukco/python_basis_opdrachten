e62358350e905ba99933cfffaa6281d8e8d53d92
luukco <97067090@st.deltion.nl>
2024-09-26 13:56:45 +0200

    modified:   opdrachten/020_tekstfuncties/opdr_2/opdr_2.py

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,10 @@
 # Opdracht 1
-# Naam student:
+# Naam student: luuk van de hoek
 # Groep:
 
 # Hier komt je code...
+my_string = ("Tinus gaat op zijn tandem naar de hottentottententoonstelling")
+letter = "t" 
+aantal = my_string.count(letter)
 
-my_list = ...
-
-print()
+print(f"De letter '{letter}' komt {aantal} keer voor in my_string ")
