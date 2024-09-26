4fd62662f2cfbc367d1c008d3a4f13a84827f026
luukco <97067090@st.deltion.nl>
2024-09-26 14:27:12 +0200

    modified:   opdrachten/030_berekeningen/opdr_1/opdr_1.py

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,12 @@
 # Groep:
 
 # Hier komt je code...
+joe = 22
+bob = 16
+fred = 22
+totaal_leeftijd = joe + bob + fred
+gemiddeld = totaal_leeftijd / 3
+
+
+print (f"De totale leeftijd van deze jongens is {totaal_leeftijd}")
+print (f"De gemiddelde leeftijd is {gemiddeld}")
