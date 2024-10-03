b5ae3e4666ec2c125dc42845ab2f0fba1eabe4c8
luukco <97067090@st.deltion.nl>
2024-10-03 14:32:02 +0200

    jup

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,17 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+
+dict_1 = {"id": 0, "voornaam": "Luuk", "achternaam": "Jansen"}
+dict_2 = {"id": 1, "voornaam": "Henk", "achternaam": "Bakker"}
+dict_3 = {"id": 2, "voornaam": "Piet", "achternaam": "de Boer"}
+dict_4 = {"id": 3, "voornaam": "Indy", "achternaam": "Visser"}
+
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+
+print(mylist[1]["voornaam"], mylist[1]["achternaam"])
