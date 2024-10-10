d9bd5c4a01403f72bdc5bc8848833a57079d7ccc
luukco <97067090@st.deltion.nl>
2024-10-10 15:16:37 +0200

    hoi

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -3,4 +3,11 @@
 # Groep:
 
 # Hier komt je code...
+steden = ["Ommen", "Zwolle", "Zalk", "Amsterdam"]
 
+steden_sorted = sorted(steden, reverse=True)
+
+
+print("De steden in omgekeerde alfabetische volgorde:")
+for stad in steden_sorted:
+    print(stad)
